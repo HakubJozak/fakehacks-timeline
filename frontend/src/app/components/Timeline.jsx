3fece876d82466857bf8017c424c9f1946744f96
Lukáš Horák <lukas.horak@ackee.cz>
2018-01-20 02:33:32 +0100

    Add image preview into timeline

--- frontend/src/app/components/Timeline.jsx
+++ frontend/src/app/components/Timeline.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import AntdTimeline from 'antd/lib/timeline';
 import { Row, Col } from 'antd/lib/grid';
+import Card from 'antd/lib/card';
 import TimelineItem from './TimelineItem';
 
 import './timeline.scss';
@@ -11,10 +12,12 @@ const Timeline = ({ checkUrl, timeline }) => {
     return (
         <Row className="timeline">
             <Row>
-                <Col offset={1}><h1>Timeline of <strong>{checkUrl}</strong></h1></Col>
-            </Row>
-            <Row>
-                <Col offset={1}>
+                <Col span={10}>
+                    <Card title={<strong title={checkUrl}>{checkUrl}</strong>} >
+                        <img src={checkUrl} alt={checkUrl} />
+                    </Card>
+                </Col>
+                <Col offset={1} span={13}>
                     <AntdTimeline>
                         {timeline.map((record, idx) =>
                             <TimelineItem key={idx} {...record} />
